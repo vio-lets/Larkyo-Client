90fa8d7b1882486d1140a39e29c02908cbb9110d
Randeep Kakar <randeepkakar@yahoo.com>
2017-05-13 16:36:41 +1200

    Separate code to App.js

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import {AppContainer} from 'react-hot-loader'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+} from 'react-router-dom'
+import Home from './screens/home'
+import LoginForm from './screens/authentication'
+import {NavigationBar} from './components'
+
+class Plan extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Plan page</h1>
+      </div>
+    )
+  }
+}
+
+class Team extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>This is Team page</h1>
+      </div>
+    )
+  }
+}
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+    exact: true
+  },
+  {
+    path: "/plan",
+    component: Plan
+  }
+]
+
+const RouteWithSubRoute = (route) => (
+  <Route path={route.path} exact={route.exact} render={props => (
+    <route.component {...props} routes={route.routes}/>
+  )}/>
+);
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router history="/">
+        <div>
+          <NavigationBar/>
+
+          {/*<div className="content">*/}
+          {/*{routes.map((route, i) => (*/}
+          {/*<RouteWithSubRoute key={i} exact={route.exact} {...route}/>*/}
+          {/*))}*/}
+          {/*</div>*/}
+
+          <div className="container">
+            <Route exact path="/" component={Home}/>
+            <Route path="/plan" component={Plan}/>
+            <Route path="/team" component={Team}/>
+            <Route path="/signin" component={LoginForm}/>
+          </div>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
